921be4345c167ab20a52c7b9aa4a8d7854f0fa31
Alejandro Coca <alxmcr@outlook.com>
2024-05-13 15:19:58 -0400

    tailwind and routes

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="bg-golden-300 size-[200px]"></div>
+    </main>
+  );
+}

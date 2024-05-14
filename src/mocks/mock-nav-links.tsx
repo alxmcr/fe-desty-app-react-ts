4cab9360ab6ee55c6cd0b863ee36b876eace9d6e
Alejandro Coca <alxmcr@outlook.com>
2024-05-14 19:30:30 -0400

    wip

---
+++ src/mocks/mock-nav-links.tsx
@@ -0,0 +1,27 @@
+import { NavLinkData } from '../@types/appTypes';
+
+export const homeLink: NavLinkData = {
+  id: 'nav-link-001',
+  text: 'Muvifav',
+  href: '/',
+};
+
+export const signUpLink: NavLinkData = {
+  id: 'nav-link-002',
+  text: 'Sign up',
+  href: '/',
+};
+
+export const signInLink: NavLinkData = {
+  id: 'nav-link-003',
+  text: 'Sign in',
+  href: '/',
+};
+
+export const newsletterLink: NavLinkData = {
+  id: 'nav-link-003',
+  text: 'Newsletter',
+  href: '#',
+};
+
+export const MOCK_NAV_LINKS: NavLinkData[] = [signInLink];

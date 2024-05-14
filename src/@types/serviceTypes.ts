0e7fa60e9d3cc42e9c3686dcb6bfeb232a95a414
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2024-05-14 01:35:11 -0400

    Chore/assets cities (#1)

---
+++ src/@types/serviceTypes.ts
@@ -0,0 +1,10 @@
+export type PlaceData = {
+  id: string;
+  name: string;
+  country: string;
+  posterLocation: string;
+  posterImageNameFile: string;
+  posterImageUrl: string;
+  posterAttributionAuthor: string;
+  posterAttributionUrl: string;
+};

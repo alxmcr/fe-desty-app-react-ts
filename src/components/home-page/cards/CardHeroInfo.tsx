5003de154e4c8a59672612712717d6acde231642
Alejandro Coca <alxmcr@outlook.com>
2024-05-14 18:28:57 -0400

    wip

--- src/components/home-page/cards/CardHeroInfo.tsx
+++ src/components/home-page/cards/CardHeroInfo.tsx
@@ -1,7 +1,7 @@
 export default function CardHeroInfo() {
   return (
     <article className="flex flex-col gap-16">
-      <div className="flex w-[70%] flex-col gap-[22px] md:w-[476px]">
+      <div className="flex w-[70%] flex-col gap-[22px] md:w-[476px] md:gap-8">
         <h1 className="font-oleo text-reef-200 text-[40px] md:text-[48px]">Desty</h1>
         <p className="font-josefin text-[24px] text-white md:text-[28px]">
           Explore the most fascinating places around the world where all of us live.

70d2e8ce932cabca05c734c3844c5b00010bbdd5
Alejandro Coca <alxmcr@outlook.com>
2024-05-15 17:15:33 -0400

    wip

--- src/components/@post/CardPost.tsx
+++ src/components/@post/CardPost.tsx
@@ -10,22 +10,29 @@ export default function CardPost({ post }: Props) {
   }
 
   return (
-    <article>
-      <header>
-        <img src={post.writer.profileImageUrl} alt={`${post.writer.name} profile phot`} />
+    <article className="flex flex-col gap-4 rounded-lg border border-reef-300 px-[12px] py-4">
+      <header className="flex gap-2">
+        <img
+          src={post.writer.profileImageUrl}
+          alt={`${post.writer.name} profile phot`}
+          className="size-[46px] rounded-full border border-reef-300 p-2"
+        />
         <div>
-          <h5 className="font-bold text-reef-300">{post.writer.name}</h5>
-          <p className="text-light-50">{post.publishedAt}</p>
+          <h5 className="font-josefin font-bold text-reef-300">{post.writer.name}</h5>
+          <p className="font-josefin text-light-50">{post.publishedAt}</p>
         </div>
       </header>
-      <div className="flex flex-col">
-        <h3 className="text-[24px] font-bold text-reef-300">{post.place.name}</h3>
-        <h4 className="text-[20px] text-light-50">{post.place.country}</h4>
+      <div className="flex flex-col gap-4">
+        <img src={post.place.posterImageUrl} alt={`${post.place.posterLocation}'s picture`} />
+        <div className="flex flex-col">
+          <h3 className="font-josefin text-[24px] font-bold text-reef-300">{post.place.name}</h3>
+          <h4 className="font-josefin text-[20px] text-light-50">{post.place.country}</h4>
+        </div>
       </div>
       <footer>
         <a
           href="#"
-          className="flex h-[48px] w-full items-center justify-center rounded-lg bg-reef-300 text-[16px] text-light-950 hover:bg-light-950 hover:text-reef-300 md:w-[152px]"
+          className="flex h-[48px] w-full items-center justify-center rounded-lg bg-reef-300 text-[16px] text-light-950 hover:bg-reef-100 hover:text-light-950 md:w-[152px]"
         >
           Read more
         </a>
